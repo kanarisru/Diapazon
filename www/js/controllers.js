0f0fe684dc5b50e1923b1cf269c50e9b620edc54
kanaris.ru <79787246978@yandex.ru>
2016-03-05 18:49:19 +0400

    allow  cors config 2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -66,6 +66,7 @@ angular.module('Diapazon.controllers', [])
       var headers = {
         'Access-Control-Allow-Origin' : '*',
         'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+        'Access-Control-Allow-Headers' : 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       };
